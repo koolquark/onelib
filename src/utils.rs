a4ae2b90a7d011281949a3ac87e512afa0150268
koolquark <koolquark@gmail.com>
2024-12-16 15:59:31 +0530

    log message

--- src/utils.rs
+++ src/utils.rs
@@ -5,7 +5,7 @@ use serde::Deserialize;
 pub fn load_config<T: for<'a> Deserialize<'a>>(env_name: &str) -> Arc<T> {
     let config_path = std::env::var(env_name).expect(
         &format!("{} environment variable not set", env_name));
-    let message = format!("Failed to open config file : {}", config);
+    let message = format!("Failed to open config file : {}", config_path);
     let mut config_file = File::open(config_path).expect(&message);
     let mut config_data = String::new();
     config_file.read_to_string(&mut config_data).expect("Failed to read config file");
