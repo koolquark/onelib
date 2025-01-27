0e8cd5ac738993f9b93b066f394f851f5d827ae5
koolquark <koolquark@gmail.com>
2025-01-27 12:27:20 +0530

    add debug and clone derives

--- src/types/outbound.rs
+++ src/types/outbound.rs
@@ -1,27 +1,29 @@
+use base64::prelude::*;
+use reqwest::{
+    header::{HeaderMap, AUTHORIZATION},
+    Certificate, Client, Identity, Proxy,
+};
 use serde::Deserialize;
 use std::time::Duration;
-use base64::prelude::*;
-use reqwest::{header::{HeaderMap, AUTHORIZATION}, Certificate, Client,Identity, Proxy};
 
 use super::basic_auth::BasicAuth;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct NamedCert {
-    pub name: String, 
-    pub cert: Certificate 
+    pub name: String,
+    pub cert: Certificate,
 }
 
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct NamedCertString {
-    pub name: String, 
-    pub pem: String
+    pub name: String,
+    pub pem: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct SmscOutboundEndpoint {
-    pub url: String, 
-    pub timeout_ms: u64, 
+    pub url: String,
+    pub timeout_ms: u64,
     pub pem_cacerts: Vec<NamedCertString>,
     pub username: String,
     pub password: String,
@@ -33,93 +35,89 @@ pub struct SmscOutboundEndpoint {
     pub auth: Option<BasicAuth>,
     pub http_proxy: Option<String>,
     pub https_proxy: Option<String>,
-
 }
 
-impl SmscOutboundEndpoint { 
-    // fn add_cacert(&mut self, data: &[u8]) { 
+impl SmscOutboundEndpoint {
+    // fn add_cacert(&mut self, data: &[u8]) {
     //     let cert: Certificate = reqwest::Certificate::from_pem(data).unwrap();
     //     self.cacerts.push(cert);
     // }
 
-    fn basic_auth_header(username: String, password: String) -> HeaderMap{
+    fn basic_auth_header(username: String, password: String) -> HeaderMap {
         let mut headers = HeaderMap::new();
         headers.insert(
             AUTHORIZATION,
-            format!("Basic {}", BASE64_STANDARD.encode(
-                format!("{}:{}", username, password)
-            )).parse().unwrap()
+            format!(
+                "Basic {}",
+                BASE64_STANDARD.encode(format!("{}:{}", username, password))
+            )
+            .parse()
+            .unwrap(),
         );
         headers
     }
 
     pub fn client(&self) -> Client {
-
         let mut builder = reqwest::ClientBuilder::new()
             .danger_accept_invalid_certs(self.accept_invalid_certs)
             .timeout(Duration::from_millis(self.timeout_ms));
 
-        for ncs in &self.pem_cacerts { 
-            let NamedCertString {name, pem}  = ncs;
+        for ncs in &self.pem_cacerts {
+            let NamedCertString { name, pem } = ncs;
             let cert: Certificate = reqwest::Certificate::from_pem(pem.as_bytes()).unwrap();
             builder = builder.add_root_certificate(cert.clone())
         }
 
         if self.identity.is_some() {
-            let identity: Identity = Identity::from_pem(self.identity.clone().unwrap().as_bytes()).unwrap();
+            let identity: Identity =
+                Identity::from_pem(self.identity.clone().unwrap().as_bytes()).unwrap();
             builder = builder.identity(identity);
         }
 
         let mut headers: HeaderMap = HeaderMap::new();
-        
-        if self.auth.is_some() { 
+
+        if self.auth.is_some() {
             let basic_auth = self.auth.clone().unwrap();
-            headers.extend(
-                Self::basic_auth_header(
-                    basic_auth.username,
-                    basic_auth.password)
-            );
+            headers.extend(Self::basic_auth_header(
+                basic_auth.username,
+                basic_auth.password,
+            ));
         }
 
-        match ( self.url.starts_with("https"), self.https_proxy.is_some(), self.http_proxy.is_some()) {
-            (true,true,_) =>  { 
-                 match Proxy::https(self.https_proxy.as_ref().unwrap()) {
-                    Ok(p) => {
-                        builder = builder.proxy(p);
-                    }
-                    Err(e) => {
-                        println!("invalid proxy settings for https_proxy: {}", e);
-                    }
+        match (
+            self.url.starts_with("https"),
+            self.https_proxy.is_some(),
+            self.http_proxy.is_some(),
+        ) {
+            (true, true, _) => match Proxy::https(self.https_proxy.as_ref().unwrap()) {
+                Ok(p) => {
+                    builder = builder.proxy(p);
                 }
-            }, 
-            (false,_,true) =>  { 
-                match Proxy::https(self.http_proxy.as_ref().unwrap()) {
-                    Ok(p) => {
-                        builder = builder.proxy(p);
-                    }
-                    Err(e) => {
-                        println!("invalid proxy settings for http_proxy: {}", e);
-                    }
+                Err(e) => {
+                    println!("invalid proxy settings for https_proxy: {}", e);
                 }
-            }, 
-            (_,_,_) => { }
-       }
-
+            },
+            (false, _, true) => match Proxy::https(self.http_proxy.as_ref().unwrap()) {
+                Ok(p) => {
+                    builder = builder.proxy(p);
+                }
+                Err(e) => {
+                    println!("invalid proxy settings for http_proxy: {}", e);
+                }
+            },
+            (_, _, _) => {}
+        }
 
         builder = builder.default_headers(headers);
 
         builder.build().unwrap()
-    
-        
     }
 }
 
-
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct OutboundEndpoint {
-    pub url: String, 
-    pub timeout_ms: u64, 
+    pub url: String,
+    pub timeout_ms: u64,
     pub pem_cacerts: Vec<NamedCertString>,
     pub auth: Option<BasicAuth>,
     pub identity: Option<String>,
@@ -134,83 +132,82 @@ fn default_useragent() -> String {
     "voice-connect".to_string()
 }
 
-impl OutboundEndpoint { 
-    // fn add_cacert(&mut self, data: &[u8]) { 
+impl OutboundEndpoint {
+    // fn add_cacert(&mut self, data: &[u8]) {
     //     let cert: Certificate = reqwest::Certificate::from_pem(data).unwrap();
     //     self.cacerts.push(cert);
     // }
 
-    fn basic_auth_header(username: String, password: String) -> HeaderMap{
+    fn basic_auth_header(username: String, password: String) -> HeaderMap {
         let mut headers = HeaderMap::new();
         headers.insert(
             AUTHORIZATION,
-            format!("Basic {}", BASE64_STANDARD.encode(
-                format!("{}:{}", username, password)
-            )).parse().unwrap()
+            format!(
+                "Basic {}",
+                BASE64_STANDARD.encode(format!("{}:{}", username, password))
+            )
+            .parse()
+            .unwrap(),
         );
         headers
     }
 
     pub fn client(&self) -> Client {
-
         let mut builder = reqwest::ClientBuilder::new()
             .danger_accept_invalid_certs(self.accept_invalid_certs)
             .timeout(Duration::from_millis(self.timeout_ms));
 
-        for ncs in &self.pem_cacerts { 
-            let NamedCertString {name, pem}  = ncs;
+        for ncs in &self.pem_cacerts {
+            let NamedCertString { name, pem } = ncs;
             let cert: Certificate = reqwest::Certificate::from_pem(pem.as_bytes()).unwrap();
             builder = builder.add_root_certificate(cert.clone())
         }
 
         if self.identity.is_some() {
-            let identity: Identity = Identity::from_pem(self.identity.clone().unwrap().as_bytes()).unwrap();
+            let identity: Identity =
+                Identity::from_pem(self.identity.clone().unwrap().as_bytes()).unwrap();
             builder = builder.identity(identity);
         }
 
         let mut headers: HeaderMap = HeaderMap::new();
-        
-        if self.auth.is_some() { 
+
+        if self.auth.is_some() {
             let basic_auth = self.auth.clone().unwrap();
-            headers.extend(
-                Self::basic_auth_header(
-                    basic_auth.username,
-                    basic_auth.password)
-            );
+            headers.extend(Self::basic_auth_header(
+                basic_auth.username,
+                basic_auth.password,
+            ));
         }
 
-        match ( self.url.starts_with("https"), self.https_proxy.is_some(), self.http_proxy.is_some()) {
-            (true,true,_) =>  { 
-                 match Proxy::https(self.https_proxy.as_ref().unwrap()) {
-                    Ok(p) => {
-                        builder = builder.proxy(p);
-                    }
-                    Err(e) => {
-                        println!("invalid proxy settings for https_proxy: {}", e);
-                    }
+        match (
+            self.url.starts_with("https"),
+            self.https_proxy.is_some(),
+            self.http_proxy.is_some(),
+        ) {
+            (true, true, _) => match Proxy::https(self.https_proxy.as_ref().unwrap()) {
+                Ok(p) => {
+                    builder = builder.proxy(p);
                 }
-            }, 
-            (false,_,true) =>  { 
-                match Proxy::https(self.http_proxy.as_ref().unwrap()) {
-                    Ok(p) => {
-                        builder = builder.proxy(p);
-                    }
-                    Err(e) => {
-                        println!("invalid proxy settings for http_proxy: {}", e);
-                    }
+                Err(e) => {
+                    println!("invalid proxy settings for https_proxy: {}", e);
                 }
-            }, 
+            },
+            (false, _, true) => match Proxy::https(self.http_proxy.as_ref().unwrap()) {
+                Ok(p) => {
+                    builder = builder.proxy(p);
+                }
+                Err(e) => {
+                    println!("invalid proxy settings for http_proxy: {}", e);
+                }
+            },
 
-            (_,_,_) => {
-
+            (_, _, _) => {
                 builder = builder.no_proxy();
-             }
-       }
+            }
+        }
 
         builder = builder.default_headers(headers);
 
         builder.build().unwrap()
-    
-        
     }
 }
--- src/types/reporter.rs
+++ src/types/reporter.rs
@@ -3,41 +3,42 @@ use std::collections::HashMap;
 use reqwest::{Client, Response};
 
 #[allow(dead_code)]
+#[derive(Debug, Clone)]
 pub struct ReporterWithResponse {
     pub client: Client,
 }
 
 impl ReporterWithResponse {
-    
     #[allow(dead_code)]
-    pub async fn report(self, uri: &str,hm: &HashMap::<String, Option<String>>) ->  Result<Response, reqwest::Error>{ 
-        self.client.post(uri)
-        .json(hm)
-        .send().await
-        
+    pub async fn report(
+        self,
+        uri: &str,
+        hm: &HashMap<String, Option<String>>,
+    ) -> Result<Response, reqwest::Error> {
+        self.client.post(uri).json(hm).send().await
     }
 
     #[allow(dead_code)]
-    pub async fn report_msg(self, uri: &str,hm: &HashMap::<String, Option<String>>, msg: &str) ->  Result<Response, reqwest::Error>{ 
-        match self.client.post(uri)
-        .json(hm)
-        .send().await {
+    pub async fn report_msg(
+        self,
+        uri: &str,
+        hm: &HashMap<String, Option<String>>,
+        msg: &str,
+    ) -> Result<Response, reqwest::Error> {
+        match self.client.post(uri).json(hm).send().await {
             Ok(v) => {
                 println!("REPORTED TO : {} : {} : {} ", uri, v.status(), msg);
                 Ok(v)
-            },
+            }
             Err(e) => {
-                println!("ERROR : {} :  {} : {}",uri, msg, e.to_string());
+                println!("ERROR : {} :  {} : {}", uri, msg, e.to_string());
                 Err(e)
             }
         }
     }
 
-    
     #[allow(dead_code)]
-    pub fn new(client: Client) -> Self { 
-        Self { 
-            client
-        }
+    pub fn new(client: Client) -> Self {
+        Self { client }
     }
 }
